b4a828ae90b937c917573935ad0cbf2b205b2cfd
Jennessa Sierra <2021153908@ub.edu.bz>
2026-04-30 13:46:02 -0600

    refactor(controller): changed filename, variable names, and added comments

--- mbot-controller/internal/config/config.go
+++
@@ -1,8 +0,0 @@
-package config
-
-const (
-	BLEModuleLocalName      = "Makeblock_LE703e97eecbe3"
-	BLEServiceUUID          = "0000ffe1-0000-1000-8000-00805f9b34fb"
-	BLERXCharacteristicUUID = "0000ffe2-0000-1000-8000-00805f9b34fb"
-	BLETXCharacteristicUUID = "0000ffe3-0000-1000-8000-00805f9b34fb"
-)
---
+++ mbot-controller/internal/settings/settings.go
@@ -0,0 +1,8 @@
+package settings
+
+const (
+	DeviceLocalName = "Makeblock_LE703e97eecbe3"             // Team 2 MBot BLE Device Name
+	ServiceUUID     = "0000ffe1-0000-1000-8000-00805f9b34fb" // MBot BLE service for Serial Communication with the MBot
+	RXUUID          = "0000ffe2-0000-1000-8000-00805f9b34fb" // BLE UUID for serial RX Communication with the MBot
+	TXUUID          = "0000ffe3-0000-1000-8000-00805f9b34fb" // BLE UUID for serial TX Communication with the MBot
+)
